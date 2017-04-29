171f4c927d52d7f2a4b7612e546dabf04c4912d0
Aleksanderp <aleksander.piciga@gmail.com>
2017-04-29 18:06:49 +0200

    zacetek dela na postavljanju stolpov

--- TD.py
+++ TD.py
@@ -1,7 +1,6 @@
 import pygame
 from random import randint
 
-
 pygame.init()
 #dolocimo velikost igralnega okna
 sirina = 800
@@ -55,7 +54,7 @@ while prvi or drugi:
 	smer = randint(1,2)
 	if smer == 1 and prvi:
 		sprememba = randint((sirina/16),(sirina//8))
-		if not start_x + sprememba > sirina:
+		if not start_x + sprememba > sirina-sirina_poti:
 			odseki.add(pot([start_x,start_y],[start_x+sprememba,start_y],sirina_poti))
 			start_x += sprememba
 		else:
@@ -76,11 +75,12 @@ if smer == 2:
 	
 
 class turrets(pygame.sprite.Sprite):
-	def __init__(self, funkcija, x, y, w = 50, h = 50):
+	def __init__(self, funkcija, x, y, active=True,w = 50, h = 50):
 		super().__init__()
 		self.funkcija = funkcija
 		self.w = w
 		self.h = h
+		self.active = active
 
 		self.image = pygame.Surface((self.w,self.h))
 		self.rect = self.image.get_rect()
@@ -89,32 +89,36 @@ class turrets(pygame.sprite.Sprite):
 
 		#1 ---> AOE
 		if funkcija == 1:
-			self.image.fill((100,0,0))
+			self.image.fill((255,0,0))
 		#2 ---> dmg
 		if funkcija == 2:
-			self.image.fill((0,0,100))
+			self.image.fill((0,0,255))
 		#3 ---> slow
 		if funkcija == 3:
-			self.image.fill((0,100,0))
+			self.image.fill((0,255,0))
 		#4 ---> ozadje menija
 		if funkcija == 4:
 			self.image.fill((100,100,100))
+	def premik(self):
+		if self.active:
+			nov_x = pygame.mouse.get_pos()[0]
+			nov_y = pygame.mouse.get_pos()[1]
+
+			self.rect.x = nov_x - (self.w // 2)
+			self.rect.y = nov_y - (self.h // 2)
 
 stolpi = pygame.sprite.Group()
-
-#pozicije stolpov v meniju na x osi, Y JE PRI VSEH ENAK 0!!!
-AOE = 0 #y = 0
-DMG = 150 #y = 0
-SLOW = 75 #y = 0
-
+meni = turrets(4,0,0,225,75,)
 #stolpi v meniju
-stolpi.add(turrets(1,AOE,0))
-stolpi.add(turrets(2,DMG,0))
-stolpi.add(turrets(3,SLOW,0))
+AOE = turrets(1,0,0,False)
+DMG = turrets(2,75,0,False)
+SLOW = turrets(3,150,0,False)
 
-#dodajanje ozadja menija
-stolpi.add(turrets(4,0,0,225,75))
+stolpi.add(AOE)
+stolpi.add(DMG)
+stolpi.add(SLOW)
 
+stolpi.add(meni)
 
 #GLAVNA WHILE ZANKA
 while delaj:
@@ -122,6 +126,21 @@ while delaj:
 	for dogodek in pygame.event.get():
 		if dogodek.type == pygame.QUIT:
 			delaj = False
+		if dogodek.type == pygame.MOUSEBUTTONUP:
+			lokacija = pygame.mouse.get_pos()
+			objekti = [i for i in stolpi if i.rect.collidepoint(lokacija)]
+			if objekti != [] and objekti[0] == AOE:
+				x = pygame.mouse.get_pos()[0]
+				y = pygame.mouse.get_pos()[1]
+				stolpi.add(turrets(1,x,y,True))
+			if objekti != [] and objekti[0] == DMG:
+				x = pygame.mouse.get_pos()[0]
+				y = pygame.mouse.get_pos()[1]
+				stolpi.add(turrets(2,x,y,True))
+			if objekti != [] and objekti[0] == SLOW:
+				x = pygame.mouse.get_pos()[0]
+				y = pygame.mouse.get_pos()[1]
+				stolpi.add(turrets(3,x,y,True))
 
 	okno.fill((200,200,255))
 	odseki.draw(okno)
@@ -130,131 +149,3 @@ while delaj:
 
 
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-import pygame
-pygame.init()
-#dolocimo velikost igralnega okna
-sirina = 800
-visina = 800
-delaj = True
-ura = pygame.time.Clock()
-okno = pygame.display.set_mode([visina,sirina])
-
-
-class pot(pygame.sprite.Sprite):
-	def __init__(self, start, end, sirina=50, barva=(255,180,10)):
-		super().__init__()
-		self.zacetek = start
-		self.konec = end
-		self.sirina = sirina
-		self.barva = barva
-
-		dolzina = abs(self.zacetek[0]-self.konec[0])+self.sirina*4
-		y_sirina = abs(self.zacetek[1]-self.konec[1])+ self.sirina*4
-		self.image = pygame.Surface((dolzina,y_sirina),pygame.SRCALPHA)
-
-		y = min(self.zacetek[1], self.konec[1])-self.sirina*2
-		x = min(self.zacetek[0], self.konec[0])-self.sirina*2
-		pygame.draw.line(self.image, self.barva, (self.zacetek[0]-x,self.zacetek[1]-y),
-						(self.konec[0]-x, self.konec[1]-y), self.sirina)
-		pygame.draw.circle(self.image,self.barva,(self.konec[0]-x,self.konec[1]-y),self.sirina//2,0)
-
-		self.rect = self.image.get_rect()
-		self.rect.x = x 
-		self.rect.y = y
-
-odseki = pygame.sprite.Group()
-
-odseki.add(pot([50,800],[50,600]))
-odseki.add(pot([50,600],[200,600]))
-odseki.add(pot([200,600],[400,600]))
-odseki.add(pot([400,600],[400,400]))
-odseki.add(pot([400,400],[600,400]))
-odseki.add(pot([600,400],[600,250]))
-odseki.add(pot([600,250],[750,250]))
-odseki.add(pot([750,250],[750,0]))
-
-class turrets(pygame.sprite.Sprite):
-	def __init__(self, funkcija, x, y, w = 50, h = 50):
-		super().__init__()
-		self.funkcija = funkcija
-		self.w = w
-		self.h = h
-
-		self.image = pygame.Surface((self.w,self.h))
-		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
-
-		#1 ---> AOE
-		if funkcija == 1:
-			self.image.fill((100,0,0))
-		#2 ---> dmg
-		if funkcija == 2:
-			self.image.fill((0,0,100))
-		#3 ---> slow
-		if funkcija == 3:
-			self.image.fill((0,100,0))
-
-stolpi = pygame.sprite.Group()
-
-stolpi.add(turrets(1,0,0))
-stolpi.add(turrets(3,75,0))
-stolpi.add(turrets(2,150,0))
-
-
-
-
-while delaj:
-	ura.tick(60)
-	for dogodek in pygame.event.get():
-		if dogodek.type == pygame.QUIT:
-			delaj = False
-
-	okno.fill((200,200,255))
-	odseki.draw(okno)
-	stolpi.draw(okno)
-	pygame.display.flip()
-
-
-pygame.quit()
-"""
